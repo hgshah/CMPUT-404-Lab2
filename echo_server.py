3019fc2e999c09994d2b3b3ec6661557ec1f934c
hgshah <hgshah@ualberta.ca>
2022-09-23 16:36:47 -0600

    Lab2

---
+++ echo_server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import socket
+import time
+import sys
+from multiprocessing import Process
+
+#define address & buffer size
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+def main():
+    try:
+       s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    except (socket.error, msg):
+        print(f'Failed to create socket. Error code: {str(msg[0])} , Error message : {msg[1]}')
+        sys.exit()
+    print('Socket created successfully')
+
+    #QUESTION 3
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    #bind socket to address
+    s.bind((HOST, PORT))
+    #set to listening mode
+    s.listen(2)
+    
+    #continuously listen for connections
+    while True:
+        print("listening")
+        conn, addr = s.accept()
+        print("Connected by", addr)
+        multi_process = Process(target = handler, args=(s, addr, conn))
+        multi_process.daemon = True
+        multi_process.start()
+        print("Started Process:", multi_process)
+        
+        #recieve data, wait a bit, then send it back
+
+def handler(s, addr, conn):
+        full_data = conn.recv(BUFFER_SIZE)
+        time.sleep(0.5)
+        conn.sendall(full_data)
+        conn.close()
+
+if __name__ == "__main__":
+    main()
---
+++ proxy_server.py
@@ -0,0 +1,74 @@
+# reference taken from lab code presented in the class
+#!/usr/bin/env python3
+from multiprocessing.dummy import Process
+from operator import truediv
+import socket
+import time
+import sys
+from multiprocessing import Process
+
+#define address & buffer size
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+def main():
+    host = 'www.google.com'
+    port = 80
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy:
+        print("proxy server started")
+        proxy.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        proxy.bind((HOST, PORT))
+        proxy.listen(1)
+   
+        while True:
+            print("listening")
+            conn, addr = proxy.accept()
+            print("Connected by", addr)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            #recieve data, wait a bit, then send it back
+                print('Connection being establishe to google')
+                remote_ip = get_remote_ip(host)
+                s.connect((remote_ip, port))
+                multi_process = Process(target = multiple_handling, args=(s, addr, conn))
+                multi_process.daemon = True
+                multi_process.start()
+                print("Started Process:", multi_process)
+                
+            conn.close()
+
+def get_remote_ip(host):
+    print(f'Getting IP for {host}')
+    try:
+        remote_ip = socket.gethostbyname( host )
+    except socket.gaierror:
+        print ('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    print (f'Ip address of {host} is {remote_ip}')
+    return remote_ip
+
+#send data to server
+def send_data(serversocket, payload):
+    print("Sending payload")    
+    try:
+        serversocket.sendall(payload.encode())
+    except socket.error:
+        print ('Send failed')
+        sys.exit()
+    print("Payload sent successfully")
+
+def multiple_handling(s, addr, conn):
+    send_full_data = conn.recv(BUFFER_SIZE)
+    print(f"Sending received data  {send_full_data} to google")
+    s.sendall(send_full_data)
+    s.shutdown(socket.SHUT_WR)
+    data = s.recv(BUFFER_SIZE)
+    print(f"Sending received data {data} to client")
+    conn.send(data)
+    
+
+
+if __name__ == "__main__":
+    main()
